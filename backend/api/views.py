f0a412663001e9641c9229eeb87ac709a877364a
Dave <sololadavid4@gmail.com>
2024-08-22 18:26:53 +0100

    student details

--- backend/api/views.py
+++ backend/api/views.py
@@ -4,7 +4,7 @@ from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated
-
+from django.http import HttpResponse
 from .serializers import StudentSerializer,TeacherSerializer
 
 
@@ -25,5 +25,25 @@ def student_list(request):
             
             return Response({'error': serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
 
-        
+    
+@api_view(['GET', 'POST'])
+def student_id(request, id):
 
+    try:
+        query = Student.objects.get(id=id)
+
+    except Student.DoesNotExist:
+        return HttpResponse(status=404)
+
+    
+    if request.method =='GET':
+        serializer = StudentSerializer(query)
+        return Response(serializer.data)
+    
+    if request.method == 'PUT':
+        serializer = StudentSerializer( query, data = request.data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
