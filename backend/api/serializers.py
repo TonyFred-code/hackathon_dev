fc0ea0f1483e057760ad4ed8cc23768481199ab7
Dave <sololadavid4@gmail.com>
2024-08-24 22:56:23 +0100

    refactor code

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -1,140 +1,140 @@
 from rest_framework import serializers
-from .models import Student, Attendance, Class, Subject, Grade, Admin
+from .models import Student
+# # Admin Serializer
+# class AdminSerializer(serializers.ModelSerializer):
+#     assigned_class = serializers.StringRelatedField()
+#     class Meta:
+#         model = Admin
+#         fields = ['name', 'is_class_teacher', 'assigned_class']
 
-# Admin Serializer
-class AdminSerializer(serializers.ModelSerializer):
-    assigned_class = serializers.StringRelatedField()
-    class Meta:
-        model = Admin
-        fields = ['name', 'is_class_teacher', 'assigned_class']
 
+# # Subject Serializer
+# class SubjectSerializer(serializers.ModelSerializer):
+#     teacher = AdminSerializer()
+#     class_name = serializers.StringRelatedField()  # Display class name as string
 
-# Subject Serializer
-class SubjectSerializer(serializers.ModelSerializer):
-    teacher = AdminSerializer()
-    class_name = serializers.StringRelatedField()  # Display class name as string
+#     class Meta:
+#         model = Subject
+#         fields = ['name', 'teacher', 'class_name']
 
-    class Meta:
-        model = Subject
-        fields = ['name', 'teacher', 'class_name']
+#     def create(self, validated_data):
+#         teacher_data = validated_data.pop('teacher')
+#         class_name_data = validated_data.pop('class_name')
 
-    def create(self, validated_data):
-        teacher_data = validated_data.pop('teacher')
-        class_name_data = validated_data.pop('class_name')
+#         # Look up the teacher by the provided name
+#         try:
+#             teacher = Admin.objects.get(name=teacher_data['name'])
+#         except Admin.DoesNotExist:
+#             raise serializers.ValidationError({"teacher": "Teacher does not exist."})
 
-        # Look up the teacher by the provided name
-        try:
-            teacher = Admin.objects.get(name=teacher_data['name'])
-        except Admin.DoesNotExist:
-            raise serializers.ValidationError({"teacher": "Teacher does not exist."})
+#         # Look up the class by the provided name
+#         try:
+#             class_name = Class.objects.get(name=class_name_data)
+#         except Class.DoesNotExist:
+#             raise serializers.ValidationError({"class_name": "Class does not exist."})
 
-        # Look up the class by the provided name
-        try:
-            class_name = Class.objects.get(name=class_name_data)
-        except Class.DoesNotExist:
-            raise serializers.ValidationError({"class_name": "Class does not exist."})
-
-        # Create the Subject with the validated data
-        subject = Subject.objects.create(teacher=teacher, class_name=class_name, **validated_data)
-        return subject
+#         # Create the Subject with the validated data
+#         subject = Subject.objects.create(teacher=teacher, class_name=class_name, **validated_data)
+#         return subject
 
 # Grade Serializer
-class GradeSerializer(serializers.ModelSerializer):
-    subject = SubjectSerializer()  # Nested SubjectSerializer to include subject details
+# class GradeSerializer(serializers.ModelSerializer):
+#     subject = SubjectSerializer()  # Nested SubjectSerializer to include subject details
 
-    class Meta:
-        model = Grade
-        fields = ['subject', 'value']
+#     class Meta:
+#         model = Grade
+#         fields = ['subject', 'value']
 
-class AttendanceSerializer(serializers.ModelSerializer):
-    student  = serializers.StringRelatedField()
-    class Meta:
-        model = Attendance
-        fields = ['student', 'date', 'status']
+# class AttendanceSerializer(serializers.ModelSerializer):
+#     student  = serializers.StringRelatedField()
+#     class Meta:
+#         model = Attendance
+#         fields = ['student', 'date', 'status']
 
 
 # Student Serializer
 class StudentSerializer(serializers.ModelSerializer):
-    class_id = serializers.StringRelatedField()  # Display class name as string
-    current_grades = GradeSerializer(many=True)  # Use GradeSerializer to include grades with subject details
-    attendance = AttendanceSerializer()
+    # class_id = serializers.StringRelatedField()  # Display class name as string
+    # current_grades = GradeSerializer(many=True)  # Use GradeSerializer to include grades with subject details
+    # attendance = AttendanceSerializer()
     class Meta:
         model = Student
-        fields = ['id', 'first_name', 'last_name', 'email','attendance', 'gender', 'date_of_birth', 'class_id', 'current_grades', 'parent_contact_info']
+        fields = ['id', 'first_name', 'last_name', 'email',
+                   'gender', 'date_of_birth', 'parent_contact_info']
 
-    def update(self, instance, validated_data):
-        # Update class_id (Class model)
-        class_data = validated_data.pop('class_id', None)
-        if class_data:
-            class_instance = Class.objects.get(name=class_data)
-            instance.class_id = class_instance
+    # def update(self, instance, validated_data):
+    #     # Update class_id (Class model)
+    #     class_data = validated_data.pop('class_id', None)
+    #     if class_data:
+    #         class_instance = Class.objects.get(name=class_data)
+    #         instance.class_id = class_instance
 
-        # Update grades (Grade model)
-        grades_data = validated_data.pop('current_grades', [])
-        for grade_data in grades_data:
-            subject_name = grade_data['subject']['name']
-            subject_instance = Subject.objects.get(name=subject_name)
-            grade_instance = instance.current_grades.get(subject=subject_instance)
-            grade_instance.value = grade_data.get('value', grade_instance.value)
-            grade_instance.save()
+    #     # Update grades (Grade model)
+    #     grades_data = validated_data.pop('current_grades', [])
+    #     for grade_data in grades_data:
+    #         subject_name = grade_data['subject']['name']
+    #         subject_instance = Subject.objects.get(name=subject_name)
+    #         grade_instance = instance.current_grades.get(subject=subject_instance)
+    #         grade_instance.value = grade_data.get('value', grade_instance.value)
+    #         grade_instance.save()
 
-        # Update other Student fields
-        for attr, value in validated_data.items():
-            setattr(instance, attr, value)
-        instance.save()
+    #     # Update other Student fields
+    #     for attr, value in validated_data.items():
+    #         setattr(instance, attr, value)
+    #     instance.save()
 
-        return instance
+    #     return instance
     
 
 
 
 
-# Class Serializer with Nested Serializers
-class ClassSerializer(serializers.ModelSerializer):
-    students = StudentSerializer(many=True, read_only=True, source='student_class')
-    subjects = SubjectSerializer(many=True, read_only=True)
+# # Class Serializer with Nested Serializers
+# class ClassSerializer(serializers.ModelSerializer):
+#     students = StudentSerializer(many=True, read_only=True, source='student_class')
+#     # subjects = SubjectSerializer(many=True, read_only=True)
 
-    class Meta:
-        model = Class
-        fields = ['id', 'name', 'students', 'subjects']
+#     class Meta:
+#         model = Class
+#         fields = ['id', 'name', 'students']
 
-    def create(self, validated_data):
-        students_data = self.initial_data.get('students', [])
-        subjects_data = self.initial_data.get('subjects', [])
+#     def create(self, validated_data):
+#         students_data = self.initial_data.get('students', [])
+#         subjects_data = self.initial_data.get('subjects', [])
 
-        class_instance = Class.objects.create(**validated_data)
+#         class_instance = Class.objects.create(**validated_data)
 
-        # Handle nested student creation
-        for student_data in students_data:
-            student_data['class_id'] = class_instance
-            Student.objects.create(**student_data)
+#         # Handle nested student creation
+#         for student_data in students_data:
+#             student_data['class_id'] = class_instance
+#             Student.objects.create(**student_data)
 
-        # Handle nested subject creation
-        for subject_data in subjects_data:
-            subject_data['class_name'] = class_instance
-            Subject.objects.create(**subject_data)
+#         # Handle nested subject creation
+#         for subject_data in subjects_data:
+#             subject_data['class_name'] = class_instance
+#             Subject.objects.create(**subject_data)
 
-        return class_instance
+#         return class_instance
 
-    def update(self, instance, validated_data):
-        # Update basic fields
-        instance.name = validated_data.get('name', instance.name)
-        instance.save()
+#     def update(self, instance, validated_data):
+#         # Update basic fields
+#         instance.name = validated_data.get('name', instance.name)
+#         instance.save()
 
-        # For the `students` and `subjects`, manage them separately.
-        students_data = self.initial_data.get('students', [])
-        subjects_data = self.initial_data.get('subjects', [])
+#         # For the `students` and `subjects`, manage them separately.
+#         students_data = self.initial_data.get('students', [])
+#         subjects_data = self.initial_data.get('subjects', [])
 
-        # Handle updating students
-        for student_data in students_data:
-            student_instance = Student.objects.get(id=student_data['id'])
-            student_instance.class_id = instance
-            student_instance.save()
+#         # Handle updating students
+#         for student_data in students_data:
+#             student_instance = Student.objects.get(id=student_data['id'])
+#             student_instance.class_id = instance
+#             student_instance.save()
 
-        # Handle updating subjects
-        for subject_data in subjects_data:
-            subject_instance = Subject.objects.get(id=subject_data['id'])
-            subject_instance.class_name = instance
-            subject_instance.save()
+#         # Handle updating subjects
+#         for subject_data in subjects_data:
+#             subject_instance = Subject.objects.get(id=subject_data['id'])
+#             subject_instance.class_name = instance
+#             subject_instance.save()
 
-        return instance
+#         return instance
--- backend/api/views.py
+++ backend/api/views.py
@@ -1,15 +1,16 @@
 from django.shortcuts import render
-from .models import  Student, Grade, Attendance, Subject, Class, Admin
+from .models import  Student
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated
 from django.http import HttpResponse
-from .serializers import StudentSerializer, AttendanceSerializer,AdminSerializer, SubjectSerializer, ClassSerializer
-
-
+from .serializers import StudentSerializer
+# , AttendanceSerializer,AdminSerializer, SubjectSerializer, ClassSerializer
+# 
 @api_view(['GET', 'POST'])
 def student_list(request):
+    
     instance = Student.objects.all()
 
     if request.method == 'GET':
@@ -28,112 +29,113 @@ def student_list(request):
     
 @api_view(['GET', 'PUT'])
 def student_id(request, id):
+    pass
+    # try:
+    #     query = Student.objects.get(id=id)
 
-    try:
-        query = Student.objects.get(id=id)
-
-    except Student.DoesNotExist:
-        return HttpResponse(status=404)
+    # except Student.DoesNotExist:
+    #     return HttpResponse(status=404)
 
     
-    if request.method =='GET':
-        serializer = StudentSerializer(query)
-        return Response(serializer.data)
+    # if request.method =='GET':
+    #     serializer = StudentSerializer(query)
+    #     return Response(serializer.data)
     
-    if request.method == 'PUT':
-        serializer = StudentSerializer( query, data = request.data)
+    # if request.method == 'PUT':
+    #     serializer = StudentSerializer( query, data = request.data)
 
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_200_OK)
-    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    #     if serializer.is_valid():
+    #         serializer.save()
+    #         return Response(serializer.data, status=status.HTTP_200_OK)
+    # return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-@api_view(['GET', 'POST'])
-def subject_list(request):
-    subjects = Subject.objects.all()
+# @api_view(['GET', 'POST'])
+# def subject_list(request):
+#     pass
+#     subjects = Subject.objects.all()
     
 
-    if request.method == 'GET':
-        serializer = SubjectSerializer(subjects, many=True)
-        return Response({'success': serializer.data}, status=status.HTTP_200_OK)
+#     if request.method == 'GET':
+#         serializer = SubjectSerializer(subjects, many=True)
+#         return Response({'success': serializer.data}, status=status.HTTP_200_OK)
     
-    elif request.method == 'POST':
-            serializer =SubjectSerializer( data=request.data)
+#     elif request.method == 'POST':
+#             serializer =SubjectSerializer( data=request.data)
 
-            if serializer.is_valid():
-                serializer.save()
-                return Response({'success': serializer.data}, status=status.HTTP_200_OK)
+#             if serializer.is_valid():
+#                 serializer.save()
+#                 return Response({'success': serializer.data}, status=status.HTTP_200_OK)
             
-            return Response({'error': serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
+#             return Response({'error': serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
 
 
-@api_view(['GET', 'POST'])
-def class_list(request):
-    if request.method == 'GET':
-        classes = Class.objects.all()
-        serializer = ClassSerializer(classes, many=True)
-        return Response(serializer.data)
+# @api_view(['GET', 'POST'])
+# def class_list(request):
+#     if request.method == 'GET':
+#         classes = Class.objects.all()
+#         serializer = ClassSerializer(classes, many=True)
+#         return Response(serializer.data)
 
-    elif request.method == 'POST':
-        serializer = ClassSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+#     elif request.method == 'POST':
+#         serializer = ClassSerializer(data=request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data, status=status.HTTP_201_CREATED)
+#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-@api_view(['GET', 'PUT', 'DELETE'])
-def class_detail(request, pk):
-    try:
-        class_instance = Class.objects.get(pk=pk)
-    except Class.DoesNotExist:
-        return Response(status=status.HTTP_404_NOT_FOUND)
+# @api_view(['GET', 'PUT', 'DELETE'])
+# def class_detail(request, pk):
+#     try:
+#         class_instance = Class.objects.get(pk=pk)
+#     except Class.DoesNotExist:
+#         return Response(status=status.HTTP_404_NOT_FOUND)
 
-    if request.method == 'GET':
-        serializer = ClassSerializer(class_instance)
-        return Response(serializer.data)
+#     if request.method == 'GET':
+#         serializer = ClassSerializer(class_instance)
+#         return Response(serializer.data)
 
-    elif request.method == 'PUT':
-        serializer = ClassSerializer(class_instance, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+#     elif request.method == 'PUT':
+#         serializer = ClassSerializer(class_instance, data=request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data)
+#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-    elif request.method == 'DELETE':
-        class_instance.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+#     elif request.method == 'DELETE':
+#         class_instance.delete()
+#         return Response(status=status.HTTP_204_NO_CONTENT)
 
-@api_view(['GET', 'POST'])
-def admin(request):
-    if request.method == 'GET':
-        admin = Admin.objects.all()
-        serializer = AdminSerializer(admin, many=True)
-        return Response({'admin': serializer.data }, status=status.HTTP_200_OK)
+# @api_view(['GET', 'POST'])
+# def admin(request):
+#     if request.method == 'GET':
+#         admin = Admin.objects.all()
+#         serializer = AdminSerializer(admin, many=True)
+#         return Response({'admin': serializer.data }, status=status.HTTP_200_OK)
     
-    elif request.method == 'POST':
-            serializer =AdminSerializer( data=request.data)
+#     elif request.method == 'POST':
+#             serializer =AdminSerializer( data=request.data)
 
-            if serializer.is_valid():
-                serializer.save()
-                return Response({'success': serializer.data}, status=status.HTTP_200_OK)
+#             if serializer.is_valid():
+#                 serializer.save()
+#                 return Response({'success': serializer.data}, status=status.HTTP_200_OK)
                 
-            return Response({'error': serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
+#             return Response({'error': serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
 
-@api_view(['GET', 'POST'])
-def attendance_list(request):
+# @api_view(['GET', 'POST'])
+# def attendance_list(request):
  
-    if request.method == 'GET':
-        attendance = Attendance.objects.all()
-        serializer = AttendanceSerializer(attendance, many=True)
-        return Response({'attendance': serializer.data }, status=status.HTTP_200_OK)
+#     if request.method == 'GET':
+#         attendance = Attendance.objects.all()
+#         serializer = AttendanceSerializer(attendance, many=True)
+#         return Response({'attendance': serializer.data }, status=status.HTTP_200_OK)
     
-    elif request.method == 'POST':
-        serializer =AttendanceSerializer( data=request.data)
+#     elif request.method == 'POST':
+#         serializer =AttendanceSerializer( data=request.data)
 
-        if serializer.is_valid():
-            serializer.save()
-            return Response({'success': serializer.data}, status=status.HTTP_200_OK)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response({'success': serializer.data}, status=status.HTTP_200_OK)
             
-        return Response({'error': serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
+#         return Response({'error': serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
 
 
