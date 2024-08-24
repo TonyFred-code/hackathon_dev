a793b532a65e7cf402f8d2d019da33b5376aac4a
Dave <sololadavid4@gmail.com>
2024-08-24 22:56:38 +0100

    refactor code

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -112,10 +112,17 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': dj_database_url.config(
+#          default=os.getenv('DATABASE_URL')
+#      )
+# }
+
 DATABASES = {
-    'default': dj_database_url.config(
-         default=os.getenv('DATABASE_URL')
-     )
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
 
 # DATABASES = {
