d8059310858ffd88bcbd52883e9501fb8c854911
Dave <sololadavid4@gmail.com>
2024-08-25 02:18:41 +0100

    add more models

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -1,12 +1,12 @@
 from django.contrib import admin
-from .models import Student
+from .models import Student, Class, Subject
 
 
 # Register your models here.
 
 # admin.site.register(Grade)
 admin.site.register(Student)
-# admin.site.register(Class)
-# admin.site.register(Subject)
+admin.site.register(Class)
+admin.site.register(Subject)
 # admin.site.register(Attendance)
 # admin.site.register(Admin)
